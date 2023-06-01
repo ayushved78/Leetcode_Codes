43df676be6856c60e65aeed11a6af7316059c4e3
Ayush Singh <ayush.ved78@gmail.com>
2023-06-01 23:56:52 +0530

    Time: 70 ms (77.19%), Space: 19.1 MB (93.42%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,27 +1,39 @@
 class Solution {
 public:
-    int shortestPathBinaryMatrix(vector<vector<int>>& g) {
-        int n=g.size();
-        if(g[0][0] || g[n-1][n-1])return -1;
-        g[0][0]=1;
-        queue<pair<int,int>>q;
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        /*
+            Approach is simple, declare the directions and return -1. mark visited as 1 and move on
+        */
+        int n = grid.size();
+//         check if the element 1 or the last is 1 or already visited
+        if(grid[0][0] || grid[n-1][n-1])
+            return -1;
+//         mark the element 1 as marked if not marked already
+        grid[0][0] = 1;
+        queue<pair<int,int>> q;
         q.push({0,0});
-        int t=0;
-        while(q.size()){
-            int k=q.size();
+        int t = 0; // this will take the count 
+        while(q.size()) {
+            int k = q.size();
             t++;
-            while(k--){
-                int x=q.front().first;
-                int y=q.front().second;
+            while(k--) {
+//                 get the x,y vals
+                int x = q.front().first;
+                int y = q.front().second;
+//                 don't forget to pop
                 q.pop();
-                if(x==n-1 && y==n-1)return t;
-                int dx[8]={1,-1,0,0,1,1,-1,-1};
-                int dy[8]={0,0,1,-1,1,-1,1,-1};
-                for(int i=0;i<8;i++){
-                    int nx=x+dx[i];
-                    int ny=y+dy[i];
-                    if(nx>=0 && nx<n && ny>=0 && ny<n && g[nx][ny]==0){
-                        g[nx][ny]=1;
+                if(x==n-1 && y==n-1)
+                    return t;
+//                 get the direction x and direction y
+                int dx[8] = {1,-1,0,0,1,1,-1,-1};
+                int dy[8] = {0,0,1,-1,1,-1,1,-1};
+//                 iterate over all directions
+                for(int i=0;i<8;i++) {
+                    int nx = x + dx[i];
+                    int ny = y + dy[i];
+                    if(nx>=0 && nx<n && ny>=0 && ny<n && grid[nx][ny]==0) {
+//                         mark the steps as marked to not return on them
+                        grid[nx][ny] = 1;
                         q.push({nx,ny});
                     }
                 }
